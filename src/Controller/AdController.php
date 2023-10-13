c281f78f2ebe3dd1baee44f1b3f313fb5ada01cd
Quenon Dylan  Ghislain <quenond@classes.local>
2023-10-13 15:29:09 +0200

    Cours 4 - collectionType et JS

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use App\Form\AnnonceType;
 use App\Repository\AdRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,58 +14,95 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
 {
+
+    /**
+     * Permet d'afficher l'ensemble des annonces du site
+     *
+     * @param AdRepository $repo
+     * @return Response
+     */
     #[Route('/ads', name: 'ads_index')]
-    public function index( AdRepository $repo): Response
+    public function index(AdRepository $repo): Response
     {
-        $ads=$repo->findAll();
+
+        $ads = $repo->findAll();
+
         return $this->render('ad/index.html.twig', [
-            'ads'=>$ads
+            'ads' => $ads
         ]);
     }
 
 
-   /**
+    /**
      * Permet d'ajouter une annonce à la bdd 
      *
      * @return Response
      */
     #[Route("/ads/new", name:"ads_create")]
-    public function create( Request $request,EntityManagerInterface $manager): Response
+    public function create(Request $request, EntityManagerInterface $manager): Response
     {
-        $ad = new Ad();
-        $form = $this->createForm(AnnonceType::class,$ad);
 
+        $ad = new Ad();
+
+        $image1=new Image();
+        $image1->setUrl("https://picsum.photos/400/200")
+            ->setCaption('Titre 1');
+
+            $ad->addImage($image1);
+        $image2=new Image();
+        $image2->setUrl("https://picsum.photos/400/200")
+            ->setCaption('Titre 2');
+
+            $ad->addImage($image2);
+  
+
+
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        // $arrayForm = $request->request->all();
+        // dump($arrayForm);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid())
         {
+            // je persiste mon objet Ad
             $manager->persist($ad);
-            //j'envoie les persistances
+            // j'envoie les persistances dans la bdd
             $manager->flush();
 
-            $this->addFlash(
-                'success',
-                "L'annonce <strong>".$ad->getTitle()."</strong> a bien été enregistrée"
-            );
+            $this->addFlash('success', "L'annonce <strong>".$ad->getTitle()."</strong> a bien été enregistrée");
 
             return $this->redirectToRoute('ads_show',[
-                'slug'=>$ad->getSlug()
+                'slug' => $ad->getSlug()
             ]);
+
         }
-
-        // dump($arrayForm);
-
 
         return $this->render("ad/new.html.twig",[
             'myForm' => $form->createView()
         ]);
 
     }
+
+
+    /**
+     * Permet d'afficher une annonce 
+     * 
+     * @param string $slug
+     * @param Ad $ad
+     * @return Response
+     */
     #[Route("/ads/{slug}", name:"ads_show")]
     public function show(string $slug, Ad $ad): Response
     {
-        return $this->render("ad/show.html.twig",[
-            'ad'=>$ad
+        // $ad = $repo->findby(["slug"=>$slug])
+
+        // dump($ad);
+
+        return $this->render("ad/show.html.twig", [
+            'ad' => $ad
         ]);
     }
+
+
 }
