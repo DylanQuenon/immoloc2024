8f4f4d1ac7ec50af8e8a0270e45c3e11f9418d1e
DylanQuenon <dylan.quenon.04@gmail.com>
2023-10-18 09:03:05 +0200

    formulaire connexion début

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountController extends AbstractController
+{
+    #[Route('/login', name: 'account_login')]
+    public function index(): Response
+    {
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+
+    /**
+     * permet de se décconecter
+     *
+     * @return void
+     */
+    #[Route('/logout', name:"account_logout")]
+    public function logout():void{
+
+    }
+}
