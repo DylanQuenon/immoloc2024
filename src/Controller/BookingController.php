bafe51155c84b91fd3f1b594690bd7d68e5e8938
DylanQuenon <dylan.quenon.04@gmail.com>
2023-11-20 11:57:15 +0100

    reservation

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -16,40 +16,63 @@ class BookingController extends AbstractController
 {
     #[Route('/ads/{slug}/book', name: 'booking_create')]
     #[IsGranted("ROLE_USER")]
-    public function book( Ad $ad,Request $request,EntityManagerInterface $manager): Response
+    public function book(Ad $ad,Request $request, EntityManagerInterface $manager): Response
     {
-        $booking=new Booking();
-        $form= $this->createForm(BookingType::class, $booking); //retourne nom de la classe namespace compris
+        $booking = new Booking();
+        $form = $this->createForm(BookingType::class, $booking);
         $form->handleRequest($request);
 
-        if($form->isSubmitted()&& $form->isValid())
+        if($form->isSubmitted() && $form->isValid())
         {
-                $user=$this->getUser();
-                $booking->setBooker($user)
-                        ->setAd($ad);
-                //si les dates ne sont pas disponible, message d'erreur
+            // traitement -> ajouter le user et l'annonce
+            $user = $this->getUser();
+            $booking->setBooker($user)
+                    ->setAd($ad);
 
-                if(!$booking->isBookableDates())
-                {
-                    $this->addFlash(
-                        'warning',
-                        'Les dates que vous avez choisie ne peuvent être réservées; elles sont déjà prises'
-                    );
-                }else{
-                    $this->addFlash(
-                        'success',
-                        "Merci pour votre réservation"
-                    );
+            // si les dates ne sont pas disponbile, message d'erreur
+            if(!$booking->isBookableDates())
+            {
+                $this->addFlash(
+                    'warning',
+                    'Les dates que vous avez choisie ne peuvent être réservées: elles sont déjà prises!'
+                );
+            }else{
+                // $this->addFlash(
+                //     'success',
+                //     "Merci pour votre réservation"
+                // );
     
-                    $manager->persist($booking);
-                    $manager->flush();
+                $manager->persist($booking);
+                $manager->flush();
 
-                }
+                return $this->redirectToRoute('booking_show',[
+                    'id'=>$booking->getId(),
+                    'withAlert' => true
+                ]);
+
+            }
+
         }
 
         return $this->render('booking/book.html.twig', [
-            "myForm"=>$form->createView(),
-            "ad"=>$ad
+            'myForm' => $form->createView(),
+            'ad' => $ad
+        ]);
+    }
+
+
+    /**
+     * Permet d'afficher la page d'un réservation
+     *
+     * @param Booking $booking
+     * @return Response
+     */
+    #[Route("/booking/{id}", name:"booking_show")]
+    #[IsGranted("ROLE_USER")]
+    public function show(Booking $booking): Response
+    {
+        return $this->render("booking/show.html.twig",[
+            'booking' => $booking
         ]);
     }
 }
