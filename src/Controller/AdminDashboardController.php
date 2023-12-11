7aa7e2474a7c655aeb76e2ce82554a6c114236b8
DylanQuenon <dylan.quenon.04@gmail.com>
2023-12-11 10:47:00 +0100

    stats service

---
+++ src/Controller/AdminDashboardController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\StatsService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminDashboardController extends AbstractController
+{
+   
+    #[Route('/admin', name: 'admin_dashboard_index')]
+    public function index(StatsService $statsService): Response
+    {
+        $users = $statsService->getUsersCount();
+        $ads = $statsService->getAdsCount();
+        $bookings = $statsService->getBookingsCount();
+        $comments = $statsService->getCommentsCount();
+
+        $bestAds = $statsService->getAdsStats('DESC');
+        $worstAds = $statsService->getAdsStats('ASC');
+      
+        return $this->render('admin/dashboard/index.html.twig', [
+            'stats' => [
+                'users' => $users,
+                'ads' => $ads,
+                'bookings' => $bookings,
+                'comments' => $comments
+            ],
+            'bestAds' => $bestAds,
+            'worstAds' => $worstAds
+        ]);
+    }
+}
